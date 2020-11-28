14a4a37e518268da6ae66f89642d5746b1cdaefb
Serhazor <sergej.dikun@gmail.com>
2020-11-28 11:17:34 +0000

    removed meta tag

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-      <meta name="viewport" content="width=device-width; initial-scale=1.0"/>
+      {/*<meta name="viewport" content="width=device-width; initial-scale=1.0"/>*/}
         <title>World's best macro calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
